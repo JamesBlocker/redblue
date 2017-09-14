a40889f529c4b000ea1556b95de038fcf2f67033
Byron Blocker <byronblocker@gmail.com>
2017-09-14 16:59:32 -0700

    add in logic for identifying triangle

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,37 @@
 $(document).ready(function() {
-    $('#play').click(function(){
-        $('.jumbotron').fadeOut(1000);
-        setTimeout(function() {
-            $('.sides').fadeIn(1500);
-        }, 1000);
-    });
+    $('form#sides').submit(function(event) {
+        var i1 = parseInt($('input#side1').val());
+        var i2 = parseInt($('input#side2').val());
+        var i3 = parseInt($('input#side3').val());
 
-    $('#doIt').click(function(event) {
-        $('.sides').fadeOut();
+        $('form').fadeOut(500);
         setTimeout(function() {
-            $('.result').fadeIn(1500);
-        }, 1000);
-        $('.jumbotron').hide();
+            $('.result').fadeIn();
+        }, 700);
+        setTimeout(function() {
+            if (i1 + i2 <= i3 || i1 + i3 <= i2 || i2 + i3 <= i1) {
+                $('.not').fadeIn();
+            } else if (i1 === i2 && i1 === i3 && i2 === i3) {
+                $('.equi').fadeIn();
+            } else if (i1 === i2 || i1 == i3 || i2 === i3) {
+                $('.isos').fadeIn();
+            } else {
+                $('.scale').fadeIn();
+            }
+
+        }, 2000);
+
+
         event.preventDefault();
     });
 });
+
+
+
+
+// $('#play').click(function(){
+//     $('.jumbotron').fadeOut(1000);
+//     setTimeout(function() {
+//         $('.sides').fadeIn(1500);
+//     }, 1000);
+// });
